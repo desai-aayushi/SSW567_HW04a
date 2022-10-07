6597a262f8ef697ce32e9c317afb5a0c8fa38f44
Aayushi Desai <aayushi1512000@gmail.com>
2022-10-07 19:48:58 -0400

    "add travis file"

--- Test_GitInfo.py
+++ Test_GitInfo.py
@@ -1,12 +1,18 @@
 import unittest
-from GitInfo import getGitRepoInfo,getNoOfCommits
+from GitInfo import getGitRepoInfo, getNoOfCommits
+
 
 class TestGitInfo(unittest.TestCase):
     def test_userId(self):
-        self.assertEqual(getGitRepoInfo(111111),"User ID is not valid")
+        self.assertEqual(getGitRepoInfo(111111), "User ID is not valid")
+
     def test_NoOfCommits(self):
-        self.assertEqual(getNoOfCommits("desai-aayushi","SSW567_HW02a"),8)
+        self.assertEqual(getNoOfCommits("desai-aayushi", "SSW567_HW02a"), 8)
+
     def test_gitRepos(self):
-        self.assertListEqual(getGitRepoInfo("desai-aayushi"),["dbmsproject","LT_Project","SSW567_HW00_HelloWorld","SSW567_HW02a"])
+        self.assertListEqual(getGitRepoInfo("desai-aayushi"),
+                             ["dbmsproject", "LT_Project", "SSW567_HW00_HelloWorld", "SSW567_HW02a"])
+
+
 if __name__ == '__main__':
     unittest.main()
